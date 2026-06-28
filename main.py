ec3d2262b6b51c0885f20c6b29b31b5d76eef964
s3ksa <simon.sachet@protonmail.com>
2026-06-28 17:58:20 +0200

    Patch

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import random
+from io import text_encoding
 from random import *
+import codecs
 
 ran = Random()
 
 while True:
-    file = open(f'{input("Quel fichier voulez-vous ouvrir ? [nom de la feuille de dates] > " )}.txt', 'r+')
-    list_date = []
+    fichier = input("Quel fichier voulez-vous ouvrir ? [nom de la feuille de dates] > " )
+    with codecs.open(f'{fichier}.txt', encoding='utf-8') as file:
+        list_date = []
 
-    for line in file.read().split('\n'):
-        date = line.split(':')
-        list_date.append(date)
+        for line in file.read().split('\r\n'):
+            date = line.split(':')
+            list_date.append(date)
 
-    file.close()
+        place = 1
+        place_q = 0
 
-    place = 1
-    place_q = 0
+        if input("Voulez vous réviser les dates ou les évènement [1 ou 2] > ") == '2':
+            place = 0
+            place_q = 1
 
-    if input("Voulez vous réviser les dates ou les évènement [1 ou 2] > ") == '2':
-        place = 0
-        place_q = 1
+        ran.shuffle(list_date)
 
-    ran.shuffle(list_date)
+        for element in list_date:
 
-    for element in list_date:
+            if element[place] == input(f"{element[place_q]} > "):
+                print("oui")
 
-        if element[place] == input(f"{element[place_q]} > "):
-            print("oui")
-
-        else:
-            print(f"Mauvaise réponse. Bonne réponse : {element[place]}")
+            else:
+                print(f"Mauvaise réponse. Bonne réponse : {element[place]}")
